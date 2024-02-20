57015ce75bc0914570edbb75022803f81274ea4c
Ikboljon Abdurasulov <ikboljon@radcode.co>
2024-02-20 17:38:02 +0100

    dynamic bug fixed

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,26 @@
-import "./globals.css"
-import UserProvider from './context/user'
-import CartProvider from './context/cart'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import "./globals.css";
+import UserProvider from "./context/user";
+import CartProvider from "./context/cart";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export const dynamic = "force-dynamic";
 export const metadata = {
-  title: 'eBay Clone',
-  description: 'eBay Clone',
-}
- 
-export default function RootLayout({ children }) {
+  title: "eBay Clone",
+  description: "eBay Clone",
+};
 
- return (
+export default function RootLayout({ children }) {
+  return (
     <html lang="en">
       <body>
         <div>
           <ToastContainer />
 
           <UserProvider>
-            <CartProvider>
-              {children}
-            </CartProvider>
+            <CartProvider>{children}</CartProvider>
           </UserProvider>
         </div>
       </body>
     </html>
-  )
+  );
 }
