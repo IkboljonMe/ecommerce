b6b575be256fb7934ffea9a202bdc13a3a3267ff
Ikboljon Abdurasulov <ikboljon@radcode.co>
2024-02-21 20:21:36 +0100

    carousel added

--- app/components/SimilarProducts.js
+++ app/components/SimilarProducts.js
@@ -1,54 +1,75 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
-import ProductComp from "./Product"
-import { BiLoader } from 'react-icons/bi'
+import { useEffect, useState } from "react";
+import ProductComp from "./Product";
+import { BiLoader } from "react-icons/bi";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-export default function SimilarProducts () {
-
-  const [products, setProducts] = useState([])
+export default function SimilarProducts() {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getRandomProducts = async () => {
     try {
-      const response = await fetch('/api/products/get-random')
-      const result = await response.json()
+      const response = await fetch("/api/products/get-random");
+      const result = await response.json();
 
       if (result) {
-        setProducts(result)
-        return
+        setProducts(result);
+        setIsLoading(false);
+        return;
       }
 
-      setProducts([])
+      setProducts([]);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error)
-      alert(error)
+      console.log(error);
+      alert(error);
     }
-  }
+  };
 
-  useEffect(() => { getRandomProducts() }, [])
+  useEffect(() => {
+    getRandomProducts();
+  }, []);
 
-  return ( 
-    <>
-        <div>
-            <div className="border-b py-1 max-w-[1200px] mx-auto" />
-
-            <div className="max-w-[1200px] mx-auto">
-                <div className="font-bold text-2xl py-2 mt-4">Similar sponsored items</div>
-
-                {products.length > 0 ?
-                  <div className="grid grid-cols-5 gap-4">
-                    {products.map(product => (
-                        <ProductComp key={product.id} product={product} />
-                    ))}
-                  </div>
-                : <div className="flex items-center justify-center">
-                      <div className="flex items-center justify-center gap-4 font-semibold">
-                          <BiLoader size={30} className="text-blue-400 animate-spin"/>
-                          Loading Products...
-                      </div>
-                  </div>}
-            </div>
+  return (
+    <div>
+      <div className="border-b py-1 max-w-[1200px] mx-auto" />
+      <div className="max-w-[1200px] mx-auto">
+        <div className=" text-center justify-center font-bold text-2xl py-2 mt-4">
+          Similar sponsored items
         </div>
-    </>
+        {isLoading ? (
+          <div className="flex items-center justify-center gap-4 font-semibold">
+            <BiLoader size={30} className="text-blue-400 animate-spin" />
+            Loading Products...
+          </div>
+        ) : (
+          <div className="hidden sm:block">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+              {products.map((product) => (
+                <ProductComp key={product.id} product={product} />
+              ))}
+            </div>
+          </div>
+        )}
+        {/* Carousel for mobile devices */}
+        {!isLoading && (
+          <div className="sm:hidden mt-4">
+            <Carousel showArrows={true} showThumbs={false} showStatus={false}>
+              {products.map((product) => (
+                <div
+                  style={{ paddingBottom: "3rem", paddingTop: "4rem" }}
+                  key={product.id}
+                >
+                  <ProductComp product={product} />
+                </div>
+              ))}
+            </Carousel>
+          </div>
+        )}
+      </div>
+    </div>
   );
-};
+}
--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import MainLayout from "../../layouts/MainLayout";
 import SimilarProducts from "../../components/SimilarProducts";
 import { useEffect, useState } from "react";
@@ -28,54 +27,57 @@ export default function Product({ params }) {
   }, []);
 
   return (
-    <>
-      <MainLayout>
-        <div className="max-w-[1200px] mx-auto">
-          <div className="flex px-4 py-10">
+    <MainLayout>
+      <div className="max-w-[1200px] mx-auto">
+        <div className="flex flex-col md:flex-row px-4 py-10">
+          <div className="w-full md:w-2/5 md:mr-8 mb-4 md:mb-0">
             {product?.url ? (
-              <img className="w-[40%] rounded-lg" src={product?.url + "/280"} />
+              <img
+                className="w-full h-auto md:max-h-[400px] lg:max-h-[500px] rounded-lg"
+                src={product?.url + "/280"}
+              />
             ) : (
-              <div className="w-[40%]"></div>
+              <div className="w-full"></div>
             )}
+          </div>
+          <div className="w-full md:w-3/5">
+            <div className="font-bold text-xl">{product?.title}</div>
+            <div className="text-sm text-gray-700 pt-2">
+              Brand New - Full Warranty
+            </div>
 
-            <div className="px-4 w-full">
-              <div className="font-bold text-xl">{product?.title}</div>
-              <div className="text-sm text-gray-700 pt-2">
-                Brand New - Full Warranty
+            <div className="border-b py-1" />
+
+            <div className="pt-3 pb-2">
+              <div className="flex items-center">
+                Condition:{" "}
+                <span className="font-bold text-[17px] ml-2">New</span>
               </div>
+            </div>
 
-              <div className="border-b py-1" />
+            <div className="border-b py-1" />
 
-              <div className="pt-3 pb-2">
+            <div className="pt-3">
+              <div className="w-full flex items-center justify-between">
                 <div className="flex items-center">
-                  Condition:{" "}
-                  <span className="font-bold text-[17px] ml-2">New</span>
+                  Price:
+                  {product?.price ? (
+                    <div className="font-bold text-[20px] ml-2">
+                      {(product?.price / 100).toFixed(2)} PLN
+                    </div>
+                  ) : null}
                 </div>
-              </div>
-
-              <div className="border-b py-1" />
-
-              <div className="pt-3">
-                <div className="w-full flex items-center justify-between">
-                  <div className="flex items-center">
-                    Price:
-                    {product?.price ? (
-                      <div className="font-bold text-[20px] ml-2">
-                        {(product?.price / 100).toFixed(2)} PLN
-                      </div>
-                    ) : null}
-                  </div>
-                  <button
-                    onClick={() => {
-                      if (cart.isItemAdded) {
-                        cart.removeFromCart(product);
-                        toast.info("Removed from cart", { autoClose: 3000 });
-                      } else {
-                        cart.addToCart(product);
-                        toast.success("Added to cart", { autoClose: 3000 });
-                      }
-                    }}
-                    className={`
+                <button
+                  onClick={() => {
+                    if (cart.isItemAdded) {
+                      cart.removeFromCart(product);
+                      toast.info("Removed from cart", { autoClose: 3000 });
+                    } else {
+                      cart.addToCart(product);
+                      toast.success("Added to cart", { autoClose: 3000 });
+                    }
+                  }}
+                  className={`
                       text-white py-2 px-20 rounded-full cursor-pointer 
                       ${
                         cart.isItemAdded
@@ -83,24 +85,23 @@ export default function Product({ params }) {
                           : "bg-[#3498C9] hover:bg-[#0054A0]"
                       }
                     `}
-                  >
-                    {cart.isItemAdded ? "Remove From Cart" : "Add To Cart"}
-                  </button>
-                </div>
+                >
+                  {cart.isItemAdded ? "Remove From Cart" : "Add To Cart"}
+                </button>
               </div>
+            </div>
 
-              <div className="border-b py-1" />
+            <div className="border-b py-1" />
 
-              <div className="pt-3">
-                <div className="font-semibold pb-1">Description:</div>
-                <div className="text-sm">{product?.description}</div>
-              </div>
+            <div className="pt-3">
+              <div className="font-semibold pb-1">Description:</div>
+              <div className="text-sm">{product?.description}</div>
             </div>
           </div>
         </div>
+      </div>
 
-        <SimilarProducts />
-      </MainLayout>
-    </>
+      <SimilarProducts />
+    </MainLayout>
   );
 }
